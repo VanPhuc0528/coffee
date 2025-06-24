e11deaed01620b27f6e305f76e92e9d28eabce85
VanPhuc0528 <phuc.2274802010686@vanlanguni.vn>
2025-06-24 13:11:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // <-- Đường dẫn tới db
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
+server.use(cors());
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.use(router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+server.use(router);
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`✅ JSON Server is running on http://localhost:${PORT}`);
 });
